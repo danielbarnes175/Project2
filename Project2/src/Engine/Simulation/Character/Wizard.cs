99ef600420c5c1dbbad00a208239430e8e3e7beb
Braedon Szolomayer <bfromoc@gmail.com>
2023-02-28 00:26:40 -0700

    rebasing

--- Project2/src/Engine/Simulation/Character/Wizard.cs
+++ Project2/src/Engine/Simulation/Character/Wizard.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
+using MiNET;
+using MonoGame.Framework.Utilities;
 using Project1.src.Engine;
 using System;
 using System.Collections.Generic;
@@ -7,6 +9,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.CompilerServices.RuntimeHelpers;
 
 namespace Project2.src.Engine.Simulation.Character
 {
@@ -26,9 +29,13 @@ namespace Project2.src.Engine.Simulation.Character
         // int, short for integer, is a type of variable that is a number without a decimal place.
         int currentMana;
         int maxMana;
+        // private bool isTeleport;
         int acceleration;
         int baseSpeed;
         public object fireball;
+        private object objectToMove;
+        private static object transform;
+        private Vector2 teleportPosition;
 
         // The constructor for our object. Whenever we want one of these, we can call code such as follows:
         // Wizard myWizard = new Wizard(...);
@@ -41,22 +48,37 @@ namespace Project2.src.Engine.Simulation.Character
             acceleration = 1;
             currentMana = 0;
             maxMana = 100;
-            // isTeleport = false;
-
+            var isTeleport = false;
         }
 
-        /**
-         * Wizard are a slower class, opting to use more spells for their movement.
-         */
-        public override void Update()
+        public class Teleport
         {
+            public Wizard wizard;
+            public Vector3 teleportPosition;
+            private bool isTeleport;
+
+            private void Update()
+            {
+                isTeleport = GlobalParameters.GlobalKeyboard.IsKeyHeldDown(Keys.LeftShift) ? true : false;
+
+            }
+
+            private void TeleportPlayer(object transform) => transform.position = teleportPosition;
 
 
-            // fireball = LoadContent(Texture2D.FromFile(@"Game\fireball"));
-            // spriteBatch.Begin();
-            //fireball = Content.Load<Texture2D>(@"graphics\fireball");
-            //base.private object LoadContent<fireball>(fireball texture2D);
-            // throw new NotImplementedException();
+
+
+
+            /**
+             * Wizard are a slower class, opting to use more spells for their movement.
+             */
+            public override void Update()
+        {
+            // Vector2 targetPosition = new Vector2(0, 0);
+            // isTeleport = GlobalParameters.GlobalKeyboard.IsKeyHeldDown(Keys.LeftShift) ? true : false;
+            //  position = teleportPosition;
+
+
 
             speed.X = speed.X > 0 ? speed.X - (int)1.5 : speed.X;
             speed.X = speed.X < 0 ? speed.X + (int)1.5 : speed.X;
@@ -87,16 +109,18 @@ namespace Project2.src.Engine.Simulation.Character
 
             base.Update();
         }
-
-
-        /* TODO:  Update the speed of this object based on input. See src/Engine/Simulation/Character/Warrior.cs as an example.
-         * 
-         ////* BONUS: Wizards have the ability to "blink" or teleport a short distance. Unfortunately, this uses their mana. Implement the ability to blink when pressing "SHIFT"
-         * Hint:  You will have to make multiple checks. Is the shift key pressed? Does the player have sufficient mana? If so, we can update the position directly here, but
-         *        make sure that we subtract the mana we've used!
-         */
-
-        // Call the Update function of the parent class, BaseCharacter.
-
     }
 }
+
+
+
+/* TODO:  Update the speed of this object based on input. See src/Engine/Simulation/Character/Warrior.cs as an example.
+* 
+  ////* BONUS: Wizards have the ability to "blink" or teleport a short distance. Unfortunately, this uses their mana. Implement the ability to blink when pressing "SHIFT"
+  * Hint:  You will have to make multiple checks. Is the shift key pressed? Does the player have sufficient mana? If so, we can update the position directly here, but
+  *        make sure that we subtract the mana we've used!
+  */
+// Call the Update function of the parent class, BaseCharacter.
+
+
+
