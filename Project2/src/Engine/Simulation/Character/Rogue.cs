9e39969ff49e83c47db7feff8eefa8f22ca0193b
Michael <MCAiello13@gmail.com>
2023-03-03 21:26:41 -0600

    Rogue movement + invisibility functionality

--- Project2/src/Engine/Simulation/Character/Rogue.cs
+++ Project2/src/Engine/Simulation/Character/Rogue.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -29,27 +31,65 @@ namespace Project2.src.Engine.Simulation.Character
         public Rogue(Vector2 position, Vector2 dimensions) : base("Assets/Game/rogue", position, dimensions)
         {
             health = 100;
-
             // Any variables listed above our constructor are called "instance variables" because they are specific to this instance of our object.
             // For example, We can have many different Rogues in the world, and they all might hold different values for position within the world.
             // So we create a separate instance of the Rogue class.
             isInvisible = false;
         }
 
-        /**
-         * Rogues are sneaky, swift scoundrels. They have slippery movement, and can move at a moderate pace, accelerating quickly to utilize their agility.
-         */
         public override void Update()
         {
-            /* TODO:  Update the speed of this object based on input. See src/Engine/Simulation/Character/Warrior.cs as an example.
-             * 
-             * BONUS: Rogues have the ability to go invisible! Don't draw the rogue if the "SHIFT" key is being held down. 
-             * Hint:  You will have to override the "Draw" function similar to how we are overriding the "Update" function here.
-             *        Update the isInvisible variable based on if SHIFT is being held down, and then check that variable in the new Draw function you create!
-             */
+            // Decrease acceleration over time --> Friction
+            speed.X = speed.X > 0 ? speed.X - 1 : speed.X;
+            speed.X = speed.X < 0 ? speed.X + 1 : speed.X;
+            speed.Y = speed.Y > 0 ? speed.Y - 1 : speed.Y;
+            speed.Y = speed.Y < 0 ? speed.Y + 1 : speed.Y;
 
+
+            if (GlobalParameters.GlobalKeyboard.GetPress("W"))
+            {
+                speed.Y -= 2;
+            }
+            if (GlobalParameters.GlobalKeyboard.GetPress("S"))
+            {
+                speed.Y += 2;
+            }
+            if (GlobalParameters.GlobalKeyboard.GetPress("A"))
+            {
+                speed.X -= 2;
+            }
+            if (GlobalParameters.GlobalKeyboard.GetPress("D"))
+            {
+                speed.X += 2;
+            }
+
+            // Cap Speed
+            if (speed.X > 10) speed.X = 10;
+            if (speed.X < -10) speed.X = -10;
+            if (speed.Y > 10) speed.Y = 10;
+            if (speed.Y < -10) speed.Y = -10;
+
+            //Invisibility Implementation
+            if (GlobalParameters.GlobalKeyboard.GetPress("SHIFT"))
+            {
+                isInvisible = true;
+            }
+            else
+            {
+                isInvisible = false;
+            }   
+            
             // Call the Update function of the parent class, BaseCharacter.
             base.Update();
+        }
+
+        //Draw
+        public override void Draw(Vector2 offset)
+        {
+            if (!isInvisible)
+            {
+                base.Draw(offset);
+            }
         }
     }
 }
